6b1746cc086a532645fc85e3121b449d2e9cfd34
Alex Berton <alexberton47@gmail.com>
2024-09-18 08:01:56 -0300

    Adição de module.css para index de contato.

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,13 +1,18 @@
 import Container from "../../components/Container";
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
+import styles from "../Contato/Contato.module.css"
 
 function Contato(){
     return(
         <>
         <Header/>
         <Container>
-        <h1>Contato Aqui!</h1>
+            <main className={styles.testSobre}>
+                <h1>Contato Aqui!</h1>
+                <p> Paragfrafo</p>
+            </main>
+        
         </Container>
         <Footer/>
         </>
