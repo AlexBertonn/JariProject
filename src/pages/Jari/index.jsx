c7f98832937e54e2531dee83fa251685fd308cd9
Alex Berton <alexberton47@gmail.com>
2024-09-22 18:03:35 -0300

    Adições básicas

--- src/pages/Jari/index.jsx
+++ src/pages/Jari/index.jsx
@@ -9,9 +9,16 @@ function Jari(){
         <>
       <Header/>
       <Container>
-      <section className={styles.homeBack}>
-        <main className={styles.home}>
-            <p>TESTE JARI</p>
+      <section className={styles.containerJari}>
+        <main className={styles.jari}>
+            <div className={styles.imgCard}></div>
+            <div className={styles.textContainer}>
+              <h1> O que é a Jari?</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sapiente beatae officia sunt accusamus! 
+                Pariatur ex excepturi recusandae assumenda fuga veniam consequuntur quod nobis. Totam assumenda, praesentium, 
+                illum ullam saepe ea tempora explicabo, quaerat expedita esse nulla fuga! Quam recusandae deleniti animi, accusantium 
+                optio possimus rem officia libero accusamus voluptatibus repellendus ducimus est iure perferendis soluta eos facilis eius. Eveniet.</p>
+            </div>
         </main>
       </section>
       </Container>
